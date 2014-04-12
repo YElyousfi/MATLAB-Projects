e279672e794d236690966b824ec3e3d7a2c29bf5
yassine <yassine@bu.edu>
2014-04-12 02:05:31 +0200

    Slope Field

---
+++ slope_field.m
@@ -0,0 +1,44 @@
+function[]=slope_field(h)
+
+values=[-2:h:2];
+A=[2*pi -4*pi;2*pi -2*pi];
+
+hold on
+for i=1:length(values)
+    for j=1:length(values)
+        dY = A*[values(i);values(j)];
+        unitV = dY/sqrt(dY(1)^2+dY(2)^2);
+        plotV = (2*h/3)*unitV;
+        plot([values(i),values(i)+plotV(1)],...
+            [values(j),values(j)+plotV(2)]);       
+    end
+end
+
+func1 = '2*pi*x-4*pi*y';
+func2 = '2*pi*x-2*pi*y';
+
+%Approximating with delta t=h^2
+
+hold on
+for i=2:3
+    t1=[0:h^i:1];
+    x_points = [];
+    y_points = [];
+    x_points(1)=1;
+    y_points(1)=0;
+    x=1;
+    y=0;
+for j=2:1:length(t1)   %i=h:h:b
+    yk=y+h^i*eval(func2); 
+    xk=x+h^i*eval(func1);
+    x=xk;
+    y=yk;
+    x_points(j)=xk; 
+    y_points(j)=yk;
+end
+plot(x_points,y_points,'r');
+
+%Approximating with delta t=h^3
+t2=[0:h^3:1];
+
+end
